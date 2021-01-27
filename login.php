24cd7af9f3b3036a88901b5275b62b954516be30
rishav9713 <39820543+rishav9713@users.noreply.github.com>
2021-01-27 20:02:43 +0530

    Update login.php

--- login.php
+++ login.php
@@ -1,19 +1,25 @@
-<?php  
-$name=$_POST["name"];//receiving name field value in $name variable  
-$password=$_POST["password"];//receiving password field value in $password variable  
-  
-echo "Welcome: $name, your password is: $password";  
+<?php 
 
-firebase.auth().signInWithEmailAndPassword(email, password)
-  .then((userCredential) => {
-    echo "Welcome: $name, your password is: $password";  
-    // Signed in
-    var user = userCredential.user;
-    // ...
-  })
-  .catch((error) => {
-    var errorCode = error.code;
-    var errorMessage = error.message;
-  });
+firebase.auth().signInAnonymously()
+ .catch(function(error) {
+  // Handle errors
+ });
+
+// $name=$_POST["name"];//receiving name field value in $name variable  
+// $password=$_POST["password"];//receiving password field value in $password variable  
+  
+// echo "Welcome: $name, your password is: $password";  
+
+// firebase.auth().signInWithEmailAndPassword(email, password)
+//   .then((userCredential) => {
+//     echo "Welcome: $name, your password is: $password";  
+//     // Signed in
+//     var user = userCredential.user;
+//     // ...
+//   })
+//   .catch((error) => {
+//     var errorCode = error.code;
+//     var errorMessage = error.message;
+//   });
 
 ?>  
